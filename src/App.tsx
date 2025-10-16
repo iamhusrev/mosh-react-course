ae161037f52198f1d7a353ea707397adc717fabc
iamhusrev <iamhusrev@gmail.com>
2025-10-16 21:16:23 +0300

    feat: refactor App component to use Button and Alert; remove ListGroup and related logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
+import { useState } from "react";
 import "./App.css";
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
-  const cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
     <>
-      <ListGroup
-        items={cities}
-        heading="List of Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Button color='warning' children="Click Me" onClick={() => setShowAlert(!showAlert)} />
+
+      {showAlert && <Alert>This is an alert</Alert>}
     </>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,7 +1,23 @@
-const Alert = () => {
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
   return (
-    <div className="alert alert-primary" role="alert">
-      A simple primary alert—check it out!
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      <strong>Holy guacamole!</strong> You should check in on some of those
+      fields below.
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+      ></button>
     </div>
   );
 };
